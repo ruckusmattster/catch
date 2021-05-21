d65be9b1ac3020f554fb122663d6b4ccfd571fb7
ruckusmattster <77196088+ruckusmattster@users.noreply.github.com>
2021-05-21 10:32:15 +0100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,19 @@
-basic.forever(function () {
-	
+input.onButtonPressed(Button.A, function () {
+    countdown = randint(5, 20)
+    while (countdown != 0) {
+        countdown += -1
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.pause(500)
+        basic.clearScreen()
+        basic.pause(500)
+    }
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(100)
+        basic.showIcon(IconNames.SmallHeart)
+        basic.pause(100)
+    }
+    basic.showString("A")
 })
+let countdown = 0
+basic.showString("A")
